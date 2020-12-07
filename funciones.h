21e8f04792a621861e6544a7b3c8f3d7f5c2187c
Mario Guerra Pérez <72316451+marioguerra2002@users.noreply.github.com>
2020-12-07 11:57:18 +0000

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int NumeroLetras (std::string palabras);
+char PrimeraLetra (std::string palabras);
+char UltimaLetra (std::string palabras);
+bool Abracadabra2 (std::string palabras);
